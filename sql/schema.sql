960f05870087230b76d1468fbd7e333041204f5e
Phil Opaola <phillipsopaolacoding@gmail.com>
2018-05-06 00:56:40 -0400

    sql/schema.sql generates more meaningful data

--- sql/schema.sql
+++ sql/schema.sql
@@ -21,13 +21,15 @@ CREATE INDEX idx_assets_category_project_id ON assets (category, project_id);
 CREATE INDEX idx_assets_category_parent_id ON assets (category, parent_id);
 
 INSERT INTO projects(id, name) VALUES
-       (1, 'Avengers'),
-       (2, 'Avengers: Age of Ultron'),
-       (3, 'Avengers: Infinity War');
+       (1, 'Project 1'),
+       (2, 'Project 2'),
+       (3, 'Project 3');
 
 INSERT INTO assets(id, name, parent_id, media_url, category, project_id) VALUES
-       (1, 'Tony Stark Bloopers', NULL, NULL, 1, 1),
-       (2, 'Tony_falls.mkv', 1, 'long_sha256_hash_here', 2, 1),
-       (3, 'Loki_Tony.mkv',  1, 'long_sha256_hash_here', 2, 1),
-       (4, 'Johannesburg Footage', NULL, NULL, 1, 2);
+       (1, 'Project 1 Folder 1', NULL, NULL, 1, 1),
+       (2, 'P1F1/File 1', 1, 'long_sha256_hash_here', 2, 1),
+       (3, 'P1F1/File 2', 1, 'long_sha256_hash_here', 2, 1),
+       (4, 'Project 1 Folder 1/Subfolder 1',  1, NULL, 1, 1),
+       (5, 'P1F1/F1/S1/File 1', 5, 'long_sha256_hash_here', 2, 1),
+       (6, 'Project 2 Folder 1', NULL, NULL, 1, 2);
 COMMIT;
